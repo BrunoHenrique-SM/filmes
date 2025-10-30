2e7355418b2d9c688119dea418da592e5611c446
BrunoHenrique-SM <brunohenrique.silvamatos@gmail.com>
2025-10-30 11:47:06 -0300

    Add files via upload

--- interface.py
+++ interface.py
@@ -1,8 +1,74 @@
 import customtkinter as ctk
 import json
 from login import *
+from tkinter import PhotoImage
+from PIL import Image, ImageTk
+corPreta = "#000000"
+corVermelha = "#db0000"
+corCinza= "#564d4d"
+corVermelhaEscura = "#831010"
+corBranca = "#ffffff"
+fonteNormal = ("Century Gothic bold",14)
+class App(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+        self.configuracoesJanelaLogin()
+        self.interfaceLogin()
+        
+        
+    def configuracoesJanelaLogin(self):
+        self.geometry("700x400")
+        self.title("Filmes")
+        self.resizable(False, False)
+        self.configure(fg_color= corPreta)
+        
+    def interfaceLogin(self):
+        
+        '''
+        self.img = ctk.CTkImage(dark_image=Image.open("duna.jpg"),
+                    size=(100, 100))
+        self.labelImagem = ctk.CTkLabel(self, text=None, image=self.img)
+        self.labelImagem.grid(row=1, column=0, padx=10 )
+        '''
+        #label
+        self.tituloUsuario = ctk.CTkLabel(self,text='Usuário ou email:',font=fonteNormal)
+        self.tituloUsuario.grid(row=0, column=0, pady=10, padx=300)
+        #entry
+        campoUsuario = ctk.CTkEntry(self,placeholder_text = " Digite o nome de usuário ou email",fg_color="#ffffff",font=fonteNormal,text_color=corPreta)
+
+        campoUsuario.grid(row=1, column=0, pady=10, padx=300)
+
+
+        #label
+        tituloSenha = ctk.CTkLabel(self,text='Senha:',font=fonteNormal)
+        tituloSenha.grid(row=2, column=0, pady=10, padx=300)
+        #entry
+        campoSenha = ctk.CTkEntry(self,placeholder_text = "Senha",fg_color="#ffffff",show="*",font=fonteNormal,text_color=corPreta)
+        campoSenha.grid(row=3, column=0, pady=10, padx=300)
+        #Botão de login
+        botaoLogin = ctk.CTkButton(self,text='Login',command='', fg_color="#db0000",hover_color=corCinza,font=fonteNormal)
+        botaoLogin.grid(row=4, column=0, pady=10, padx=300)
+        
+        #Botão de cadastro
+        botaoCadastro = ctk.CTkButton(self,text='Cadastro',command='', fg_color="#db0000",hover_color=corCinza,font=fonteNormal)
+        botaoCadastro.grid(row=5, column=0, pady=10, padx=300)
+        
+        
+        erroUsuario = ctk.CTkLabel(self,text='')
+        erroUsuario.grid(row=6, column=0, pady=10, padx=300)
+        erroSenha = ctk.CTkLabel(self,text='')
+        erroSenha.grid(row=7, column=0, pady=10, padx=300)
+        
+        
+        
+        
+        
+if __name__=="__main__":
+    app = App()
+    app.mainloop()
+'''
 class Interface:
-    #def capturaLogin():
+    
         
         
     def capturaLogin(campoUsuario,campoSenha):
@@ -10,14 +76,23 @@ class Interface:
         senha = campoSenha.get()
         return usuario,senha
     
-    def confirmaLogin(usuario,senha,usuarios):
+    def confirmaLogin(app,usuario,senha,usuarios):
+        
         usuario,senha = Interface.capturaLogin(usuario,senha)
         confirmacaoUsuario,id = Login.confirmaUsuario(usuario,usuarios)
         if confirmacaoUsuario == False:
             confirmacaoEmail,id = Login.confirmaEmail(usuario,usuarios)
         if id == -1:
-            return print("Usuário inválido")
-        return Login.confirmaSenha(senha,id,usuarios)
+            erroUsuario.configure(text="Usuário inválido",
+            text_color="red")
+        confirmacaoSenha = Login.confirmaSenha(senha,id,usuarios)
+        if confirmacaoSenha:
+            #entra no programa
+            print("Logado com sucesso")
+        else:
+            #repete processo
+            erroSenha.configure(text="Senha errada",
+            text_color="red")
         
      
 
@@ -53,17 +128,22 @@ campoUsuario.pack()
 tituloSenha = ctk.CTkLabel(app,text='Senha:')
 tituloSenha.pack(pady=20)
 #entry
-campoSenha = ctk.CTkEntry(app,placeholder_text = "Senha",fg_color="#ffffff")
+campoSenha = ctk.CTkEntry(app,placeholder_text = "Senha",fg_color="#ffffff",show="*")
 campoSenha.pack()
 
 
 
 
 
-botaoLogin = ctk.CTkButton(app,text='Login',command=lambda:Interface.confirmaLogin(campoUsuario,campoSenha,usuarios), fg_color="#db0000")
+botaoLogin = ctk.CTkButton(app,text='Login',command=lambda:Interface.confirmaLogin(app,campoUsuario,campoSenha,usuarios), fg_color="#db0000")
 
 botaoLogin.pack(pady=50)
+erroUsuario = ctk.CTkLabel(app,text='')
+erroUsuario.pack(pady = 10)
+erroSenha = ctk.CTkLabel(app,text='')
+erroSenha.pack(pady=10)
 
 #button
 app.mainloop()
+'''
 
